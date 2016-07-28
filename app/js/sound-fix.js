5826c37df9ac7640fe806924550730ade06832b6
Maxim Kraev <maxim.kraev@gmail.com>
2016-07-28 17:20:25 +0100

    fix old howler use

--- app/js/sound-fix.js
+++ app/js/sound-fix.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name        fuck flowdock
 // @namespace   flocwdock.com
-// @include     https://www.flowdock.com/app/*
+// @include     https://www.flowdock.com/*
 // @version     1
 // @grant       none
 // ==/UserScript==
 onload = () => {
   const timeout = time => {
     return () => {
-      var promise = new Promise((resolve, reject) => {
+      let promise = new Promise((resolve, reject) => {
         setTimeout(() => {
           resolve()
         }, time)
@@ -18,25 +18,51 @@ onload = () => {
     }
   }
 
-  timeout(1000)()
-  .then(() => {
-    (function (win) {
-      console.log('fuck flowdock');
-      var orig = win.Howl.prototype.play;
-      win.Howl.prototype.play = () => {
-        var self = this;
-        return win.Howler.ctx.resume().then(() => {
-          console.log('fuck flowdock play');
-            return orig.apply(self, arguments);
-        }).then(timeout(3000))
-        .then(() => {
-            return windows.setTimeout(() => {
-              return win.Howler.ctx.suspend();
-          })
-        });
-      };
+  const onEnd = (stream) => {
+    let promise = new Promise((resolve, reject) => {
+      stream.on('end', resolve)
+    })
 
-      win.Howler.ctx.suspend()
-    })(window);
+    return promise
+  }
+
+
+  let currentPlay = 0;
+  const fixPlay = (win) => {
+      console.log('fuck flowdock for not using the latest');
+      let orig = win.Howl.prototype.play;
+      let suspendPromise = win.Howler.ctx.suspend();
+
+      win.Howl.prototype.play = function() {
+        let self = this
+        let args = arguments
+        currentPlay++        
+        return suspendPromise.then(() => {
+          if (currentPlay === 1)
+            return win.Howler.ctx.resume()
+        })
+        .then(() => {
+          console.log('fuck flowdock play:', currentPlay);
+          return orig.apply(self, arguments);
+        })
+        .then(() => {
+          return onEnd(self)
+        })
+        .then(timeout(2000)) //yes this shit because one the event fired as first finishes play
+        .then(() => {
+          currentPlay--
+          console.log('fuck flowdock play end:', currentPlay);
+          if (currentPlay === 0) {  
+            suspendPromise = win.Howler.ctx.suspend();
+          }
+        })
+      };
+  }
+
+  // wait the page to finish initialize
+
+  timeout(2000)()
+  .then(() => {
+    fixPlay(window)
   });
 }
